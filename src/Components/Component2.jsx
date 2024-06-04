008854e2d4c4465a139e598c64010831406e4129
saitamachoudhary <165796559+saitamachoudhary@users.noreply.github.com>
2024-06-04 05:53:30 +0000

    secondcommit

--- src/Components/Component2.jsx
+++ src/Components/Component2.jsx
@@ -2,28 +2,61 @@ import React from "react";
 import '../Style/Component2.css';
 import {GoogleOutlined,FacebookOutlined} from '@ant-design/icons';
 import {Form,Input,Button,ConfigProvider} from 'antd';
+import {useFormik} from 'formik';
 
 const Component2=()=>{
+   const formIK=useFormik({
+      initialValues:{
+        FirstName:'',
+        LastName:'',
+        Email:'',
+        Password:'',
+      },
+      onSubmit:values=>{
+         alert(JSON.stringify(values,null,2));
+         console.log(JSON.stringify(values,null,2));
+      },
+   });
     return (
        <div className="Component2">
          <div className="formcontainer">
             <h1 style={{color:'black',marginBottom:'30px'}}>Create Account</h1>
-             <Form>
+             <Form onFinish={formIK.handleSubmit}>
                <div className="form1">
                <Form.Item>
-                  <Input style={{width:'280px'}} placeholder="FirstName"/>
+                  <Input style={{width:'280px'}} placeholder="FirstName"
+                  id="FirstName"
+                  name="FirstName"
+                  type="text"
+                  onChange={formIK.handleChange}
+                  value={formIK.values.FirstName}/>
                 </Form.Item>
                 <Form.Item>
-                  <Input style={{width:'280px'}} placeholder="LastName"/>
+                  <Input style={{width:'280px'}} placeholder="LastName"
+                    id="LastName"
+                    name="LastName"
+                    type="text"
+                    onChange={formIK.handleChange}
+                    value={formIK.values.LastName}/>
                 </Form.Item>
                </div>
                <Form.Item>
-                  <Input placeholder="Email"/>
+                  <Input placeholder="Email"
+                    id="Email"
+                    name="Email"
+                    type="email"
+                    onChange={formIK.handleChange}
+                    value={formIK.values.Email}/>
                 </Form.Item>
                 <Form.Item>
-                  <Input placeholder="Password"/>
+                  <Input placeholder="Password"
+                    id="Password"
+                    name="Password"
+                    type="password"
+                    onChange={formIK.handleChange}
+                    value={formIK.values.Password}/>
                 </Form.Item>
-                <Button className="Button" type="primary">Create Account</Button>
+                <Button className="Button" type="primary" htmlType="submit">Create Account</Button>
              </Form>
              <p style={{color:'black',fontSize:'12px',marginTop:'5px',marginBottom:'20px'}}>Have you already have an account?{" "}<a style={{color:'blue',textDecoration:'none'}} href="">Login</a></p>
              <div className="comptLine">
